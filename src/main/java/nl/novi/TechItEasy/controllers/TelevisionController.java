151382c26fa84d900c124e6f472bd5fdf59dc0db
edwinhelmus <edwin.helmus@home.nl>
2025-06-14 12:49:29 +0200

    Repository toegevoegd

--- src/main/java/nl/novi/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/nl/novi/TechItEasy/controllers/TelevisionController.java
@@ -1,78 +1,71 @@
 package nl.novi.TechItEasy.controllers;
 
 import nl.novi.TechItEasy.exceptions.InvalidTitle;
+import nl.novi.TechItEasy.models.Television;
+import nl.novi.TechItEasy.repositories.TelevisionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Optional;
+
+import static nl.novi.TechItEasy.controllers.Common.constructURI;
 
 @RestController
 @RequestMapping("/televisions")
 public class TelevisionController {
 
-    private List<String> televisionDataBase;
+    private final TelevisionRepository repos;
 
-    public TelevisionController() {
-        televisionDataBase = new ArrayList<>();
+    public TelevisionController(TelevisionRepository repos  ) {
+        this.repos = repos;
     }
 
     @GetMapping
     public ResponseEntity<Object> televisions() {
-        return ResponseEntity.ok(televisionDataBase.toString());
+        return ResponseEntity.ok(repos.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> televisions(@PathVariable int id) {
-        String television;
-        try {
-            television = televisionDataBase.get(--id);
-        }
-        catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.notFound().build();
-        }
-
-        if (television != null) {
-            return ResponseEntity.ok(television);
+    public ResponseEntity<Object> televisions(@PathVariable long id) {
+        Optional<Television> op = repos.findById(id);
+        if (op.isPresent()) {
+            return ResponseEntity.ok(op.get());
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
     @PostMapping
-    public ResponseEntity<Object> addTelevision(@RequestBody String title) {
-
-        if (title.length() > 20) {
-            throw new InvalidTitle("Title must contain at most 20 characters");
+    public ResponseEntity<Object> addTelevision(@RequestBody Television television) {
+        if (television.getName().length() > 20) {
+            throw new InvalidTitle("Name must contain at most 20 characters");
         }
-        televisionDataBase.add(title);
-
-
-        return ResponseEntity.created(null).body("Television id: " + (televisionDataBase.size()));
+        this.repos.save(television);
+        return ResponseEntity.created(constructURI(television.getId())).body(television);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateTelevision(@PathVariable int id, @RequestBody String title) {
-        try {
-            televisionDataBase.set(--id, title);
-        }
-        catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.noContent().build();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteTelevision(@PathVariable int id) {
-        try {
-            televisionDataBase.set(--id, null);
-        }
-        catch (IndexOutOfBoundsException e) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.noContent().build();
-    }
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Object> updateTelevision(@PathVariable int id, @RequestBody String title) {
+//        try {
+//            televisionDataBase.set(--id, title);
+//        }
+//        catch (IndexOutOfBoundsException e) {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        return ResponseEntity.noContent().build();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Object> deleteTelevision(@PathVariable int id) {
+//        try {
+//            televisionDataBase.set(--id, null);
+//        }
+//        catch (IndexOutOfBoundsException e) {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        return ResponseEntity.noContent().build();
+//    }
 }
 
